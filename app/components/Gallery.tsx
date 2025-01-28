a89edc917e1a1295e90137d215b3a4cc5db88832
Zoe Daniels <7177924+zkdan@users.noreply.github.com>
2025-01-28 17:31:09 -0500

    🙄

--- app/components/Gallery.tsx
+++ app/components/Gallery.tsx
@@ -24,7 +24,7 @@ export default function Gallery({ filter }: { filter: string }) {
     <>
       <Nav />
       <ul className="flex flex-wrap mx-10 md:mx-20 mt-5 ">
-        {/* {data.map(img => {
+        {data.map(img => {
           return <div key={img.id} className="mb-10  w-[100%] sm:w-[50%] md:w-[25%] ">
             <p className="capitalize text-gray-600  p-1">{`${img.name} ${img.year}`}</p>
             <Link
@@ -50,7 +50,7 @@ export default function Gallery({ filter }: { filter: string }) {
               />
             </Link>
           </div>
-        })} */}
+        })}
 
       </ul>
     </>
